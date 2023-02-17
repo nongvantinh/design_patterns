5ab4ce10b536d2981b80f1d42efc6f0b86ad5788
Nong Van Tinh <vannongtinh@gmail.com>
2023-02-17 09:59:13 +0700

    draft

---
+++ 12.compound copy/pizza_store.cpp
@@ -0,0 +1,14 @@
+#include "pizza_store.h"
+
+#include "Catch.hpp"
+
+
+using namespace pizza_store_factory;
+
+TEST_CASE("", "[]")
+{
+    SECTION("")
+    {
+        REQUIRE(" " == " ");
+    }
+}
---
+++ 12.compound copy/pizza_store.h
@@ -0,0 +1,11 @@
+#ifndef PIZZA_STORE_H
+#define PIZZA_STORE_H
+#include <string>
+#include <memory>
+
+namespace pizza_store_factory
+{
+    using String = std::string;
+
+}
+#endif // !PIZZA_STORE_H
